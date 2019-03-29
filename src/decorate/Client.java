98fdc1bc76a0bb8e6db1c7a1b01bf4cdc6ef0fd0
smallnancy <smallnancy@outlook.com>
2019-03-29 11:48:37 +0800

    装饰器模式

--- src/decorate/Client.java
+++ src/decorate/Client.java
@@ -3,6 +3,8 @@ package decorate;
 /**
  * @Author: Nancy
  * @Date: 2019/3/29 10:41
+ * 装饰器模式动态的为对象增加新的功能
+ * 抽象接口、真实角色，装饰角色
  */
 public class Client {
     public static void main(String[] args) {
